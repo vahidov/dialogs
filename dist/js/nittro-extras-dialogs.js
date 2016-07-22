0e39c90e2abe4415201d30b489c1896b638675a2
danik <me@subsonic.cz>
2016-07-22 19:49:29 +0200

    fix parent constructor call in DI bridge

--- dist/js/nittro-extras-dialogs.js
+++ dist/js/nittro-extras-dialogs.js
@@ -373,10 +373,14 @@ _context.invoke('Nittro.Extras.Dialogs', function(Dialog, DOM, Arrays) {
     Arrays: 'Utils.Arrays'
 });
 ;
-_context.invoke('Nittro.Extras.Dialogs.Bridges', function() {
+_context.invoke('Nittro.Extras.Dialogs.Bridges', function(Nittro) {
+
+    if (!Nittro.DI) {
+        return;
+    }
 
     var DialogsDI = _context.extend('Nittro.DI.BuilderExtension', function(containerBuilder, config) {
-        DialogsDI.Super.call(containerBuilder, config);
+        DialogsDI.Super.call(this, containerBuilder, config);
     }, {
         load: function() {
             this._getContainerBuilder().addFactory('formDialog', 'Nittro.Extras.Dialogs.FormDialog()');
--- src/js/Nittro/Extras/Dialogs/Bridges/DialogsDI.js
+++ src/js/Nittro/Extras/Dialogs/Bridges/DialogsDI.js
@@ -5,7 +5,7 @@ _context.invoke('Nittro.Extras.Dialogs.Bridges', function(Nittro) {
     }
 
     var DialogsDI = _context.extend('Nittro.DI.BuilderExtension', function(containerBuilder, config) {
-        DialogsDI.Super.call(containerBuilder, config);
+        DialogsDI.Super.call(this, containerBuilder, config);
     }, {
         load: function() {
             this._getContainerBuilder().addFactory('formDialog', 'Nittro.Extras.Dialogs.FormDialog()');
