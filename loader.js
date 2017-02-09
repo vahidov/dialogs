52a5ec2e4d2c452724ae1b356d55077d558082ef
danik <me@subsonic.cz>
2017-02-10 00:33:25 +0100

    Refactoring ahead of 2.0

--- loader.js
+++
@@ -1,4 +0,0 @@
-/* jahudka/minify compatible loader */
-/*! @include file="src/js/Nittro/Extras/Dialogs/Dialog.js" */
-/*! @include file="src/js/Nittro/Extras/Dialogs/FormDialog.js" */
-/*! @include file="src/js/Nittro/Extras/Dialogs/Manager.js" */
